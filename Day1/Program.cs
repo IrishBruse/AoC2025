58f635e133299d7bba7957bba9d4fcb07b42636e
Ethan Conneely <econn50@outlook.com>
2025-12-01 17:22:22 +0000

    Day 1 Part 1

---
+++ Day1/Program.cs
@@ -0,0 +1,28 @@
+﻿var lines = File.ReadAllLines("input/input1");
+
+
+int password = 0;
+int position = 50;
+Console.WriteLine(position);
+
+foreach (var line in lines)
+{
+    int number = int.Parse(line[1..]);
+
+    if (line[0] == 'L') number = -number;
+
+    position += number;
+
+    position %= 100;
+    if (position < 0) position += 100;
+
+    Console.WriteLine(position);
+
+    if (position == 0)
+    {
+        password++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(password);
