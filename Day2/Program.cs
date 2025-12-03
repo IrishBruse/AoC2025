d0757ded8e1bfbcba8222e08dcc5c1a716e3115d
Ethan Conneely <econn50@outlook.com>
2025-12-03 12:14:26 +0000

    Day 2 Part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,35 +1,77 @@
-﻿// var text = File.ReadAllText("input/sample.txt");
-var text = File.ReadAllText("input/input1.txt");
-var ranges = text.Split(',');
+﻿Console.Clear();
 
-long invalidSum = 0;
+var sample = File.ReadAllText("input/sample.txt");
+var input = File.ReadAllText("input/input1.txt");
 
-foreach (var range in ranges)
+Console.WriteLine("1188511885: " + IsInvalid("1188511885"));
+Console.WriteLine("1188511882: " + IsInvalid("1188511882"));
+Console.WriteLine("111: " + IsInvalid("111"));
+Console.WriteLine("113: " + IsInvalid("113"));
+
+Console.WriteLine("sample " + Run(sample));
+Console.WriteLine("input " + Run(input));
+
+static long Run(string input)
 {
-    string[] minxmax = range.Split("-");
+    long invalidSum = 0;
+    var ranges = input.Split(',');
 
-    string minText = minxmax[0];
-    long min = long.Parse(minText);
-
-    string maxText = minxmax[1];
-    long max = long.Parse(maxText);
-
-    Console.WriteLine("# " + min + " " + max);
-
-    for (long id = min; id <= max; id++)
+    foreach (var range in ranges)
     {
-        string idText = id.ToString();
-        if (idText.Length % 2 != 0) continue;
+        string[] minxmax = range.Split("-");
 
-        int halfLen = idText.Length / 2;
-        if (idText[..halfLen] == idText[halfLen..])
+        string minText = minxmax[0];
+        long min = long.Parse(minText);
+
+        string maxText = minxmax[1];
+        long max = long.Parse(maxText);
+
+        for (long id = min; id <= max; id++)
         {
-            Console.WriteLine(id);
-            invalidSum += id;
+            string idText = id.ToString();
+
+            if (IsInvalid(idText))
+            {
+                Console.WriteLine(id);
+                invalidSum += id;
+            }
+
         }
     }
 
+    return invalidSum;
 }
 
-Console.WriteLine();
-Console.WriteLine("invalidSum: " + invalidSum);
+static bool IsInvalid(string idText)
+{
+    if (idText.Length == 1) return false;
+
+    int halfLen = (int)MathF.Ceiling(idText.Length / 2f);
+
+    for (int w = 1; w <= halfLen; w++)
+    {
+        string pattern = idText[0..w];
+
+        if (idText.Length % w != 0) continue;
+
+        bool valid = true;
+        for (int i = w; i < idText.Length; i += w)
+        {
+            int end = i + w;
+            var range = idText[i..end];
+
+            if (range != pattern)
+            {
+                valid = false;
+                break;
+            }
+        }
+
+        if (valid)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
